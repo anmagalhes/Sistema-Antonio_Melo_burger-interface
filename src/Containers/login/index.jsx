e5f578e1fbaa76d0663cd77cb4437ea6e8b81383
anmagalhaes <rhavitony@gmai.com>
2022-03-13 22:26:09 -0300

    Atualização Index login

--- src/Containers/login/index.jsx
+++ src/Containers/login/index.jsx
@@ -1,8 +1,12 @@
 /* eslint-disable react-hooks/rules-of-hooks */
-/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable no-console */
 /* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable react/jsx-props-no-spreading */
 import React from "react"
 import { useForm } from "react-hook-form"
+
+import { yupResolver } from "@hookform/resolvers/yup"
+import * as Yup from "yup"
 
 import LoginImg from "../../assets/Login_imagem.svg"
 import Logo from "../../assets/logo.svg"
@@ -18,11 +22,18 @@ import {
 } from "./styles"
 
 function login() {
+  const schema = Yup.object().shape({
+    email: Yup.string().email().required(),
+    password: Yup.string().required()
+  })
+
   const {
     register,
     handleSubmit,
     formState: { errors }
-  } = useForm()
+  } = useForm({
+    resolver: yupResolver(schema)
+  })
 
   const onSubmit = data => console.log(data)
 
@@ -36,13 +47,14 @@ function login() {
         <form onSubmit={handleSubmit(onSubmit)}>
           <Label>Email</Label>
           <Input type="email" {...register("emails")} />
+          <p> {errors.email?.message} </p>
 
           <Label>Senha</Label>
           <Input type="password" {...register("password")} />
+          <p> {errors.password?.message} </p>
 
           <Button type="Sumit">Sign In</Button>
         </form>
-
         <SignInLink>
           Não possui conta ? <a>SignUp</a>
         </SignInLink>
